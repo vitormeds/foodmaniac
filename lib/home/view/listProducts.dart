91270df4172f2e5a848e8addee7c705d1e355d04
Vitor Mendes <vitor.mendes@zup.com.br>
2020-02-05 14:09:37 -0300

    list

--- lib/home/view/listProducts.dart
+++ lib/home/view/listProducts.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:foodmaniac/addProductWidget.dart';
 import 'package:foodmaniac/home/bloc/productBloc.dart';
 import 'package:foodmaniac/home/model/product.dart';
 
@@ -33,7 +34,12 @@ class _ListProductsWidgetState extends State<ListProducts> {
     return Scaffold(
       appBar: AppBar(title: Text("Food Maniac"),),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddProductWidget()),
+          );
+        },
         child: Icon(Icons.add),
       ),
 
@@ -53,19 +59,54 @@ class _ListProductsWidgetState extends State<ListProducts> {
           itemCount: characters.length,
           itemBuilder: (BuildContext context, int index) {
             Result character = characters[index];
-            return ListTile(
-              leading: CachedNetworkImage(
-                placeholder: (context, url) => CircularProgressIndicator(),
-                imageUrl:
-                character.image,
-              ),
-              title: Text(character.name),
-              subtitle: Text(character.species),
-            );
+           return Container(child: Row(children: <Widget>[
+             Column(
+                children: <Widget>[
+                  CachedNetworkImage(
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    imageUrl:
+                    character.image,
+                    height: 50,
+                    width: 50,
+                  )
+                ]),
+            Column(
+              children: <Widget>[
+                Row(children: <Widget>[Text(character.name,overflow: TextOverflow.clip,)],),
+                Row(children: <Widget>[Text(character.species,overflow: TextOverflow.clip,)],)
+              ]),
+            Column(
+            children: <Widget>[
+              Row(children: <Widget>[
+                RawMaterialButton(
+                  onPressed: () {},
+                  child: new Icon(
+                    Icons.add,
+                    color: Colors.blue,
+                    size: 5.0,
+                  ),
+                  shape: new CircleBorder(),
+                  elevation: 2.0,
+                  fillColor: Colors.white,
+                ),
+                Text("0"),
+                RawMaterialButton(
+                  onPressed: () {},
+                  child: new Icon(
+                    Icons.minimize,
+                    color: Colors.blue,
+                    size: 5.0,
+                  ),
+                  shape: new CircleBorder(),
+                  elevation: 2.0,
+                  fillColor: Colors.white,
+                )
+              ],)
+            ])
+           ]));
           }
         );
       }),
-
     );
   }
 }
